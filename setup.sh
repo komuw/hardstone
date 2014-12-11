166fe2a69ea62e4b44b286fe682aa5a7f0f87ffc
Komu <komuw05@gmail.com>
2014-12-11 16:53:58 +0300

    initial

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Set up Ansible on a Vagrant Ubuntu box, and run 
+# playbook/s to setup/provision
+
+echo "starting setup/provisioning...."
+
+if [[ ! $(ansible --version 2> /dev/null) =~ 1\.6 ]]; then
+        sudo apt-get update && \
+	sudo apt-get -y install python-software-properties && \
+	sudo add-apt-repository -y ppa:rquillo/ansible && \
+        sudo apt-get update && \
+	sudo apt-get -y install ansible
+fi
+
+PYTHONUNBUFFERED=1 ansible-playbook main.yml -i dev 
