2626be9278e52a35665e272092d9b80ee9178eed
komu <komuw05@gmail.com>
2016-12-06 16:20:02 +0300

    update for ubuntu 16.04

--- setup.sh
+++ setup.sh
@@ -6,12 +6,17 @@
 echo "starting setup/provisioning...."
 
 if [[ ! $(ansible --version 2> /dev/null) =~ 1\.6 ]]; then
-        sudo apt-get update && \
-	sudo apt-get -y install python-software-properties && \
-        sudo apt-get -y install software-properties-common && \
-	sudo add-apt-repository -y ppa:rquillo/ansible && \
-        sudo apt-get update && \
-	sudo apt-get -y install ansible
+    sudo apt-get update && \
+    sudo apt-get -y install gcc build-essential libssl-dev libffi-dev python-dev && \
+    sudo apt-get -y install python-software-properties && \
+    sudo apt-get -y install software-properties-common && \
+    sudo apt-get -y install python-pip && \
+    sudo pip install --ignore-installed -U pip && \
+    sudo pip install --ignore-installed jinja2 && \
+    sudo pip install --ignore-installed paramiko && \
+    sudo apt-get update && \
+    sudo apt-get -y install ansible && \
+    sudo pip install ansible
 fi
 
 PYTHONUNBUFFERED=1 ansible-playbook main.yml -i dev --connection=local #-vvvvv
