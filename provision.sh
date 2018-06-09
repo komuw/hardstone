5fe04545ac5eb567c553c4face2ac7ed20b0b2fa
komu <komuw05@gmail.com>
2018-06-09 14:47:15 +0300

    rxit file on failure

--- provision.sh
+++ provision.sh
@@ -35,10 +35,9 @@ printf "\n\n add sublime-text3 ppa\n"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 
-#transmission bittorrent, ffmpeg, mpv. add-apt-repository takes one repo as arg
+#transmission bittorrent, mpv. add-apt-repository takes one repo as arg
 printf "\n\n add some ppas\n"
 add-apt-repository -y ppa:eugenesan/ppa
-add-apt-repository -y  ppa:jonathonf/ffmpeg-3
 add-apt-repository -y  ppa:mc3man/mpv-tests
 
 printf "\n\n update cache\n"
