10027cf5d7284990568aa532d6a874f8d53c2efc
komuW <komuw05@gmail.com>
2018-07-08 22:07:09 +0300

    template copy

--- vscode.sh
+++ vscode.sh
@@ -18,62 +18,10 @@ dpkg -i /tmp/index.html\?LinkID\=760868
 
 # on MacOs it is /Users/komuw/Library/Application\ Support/Code/User/settings.json
 printf "\n\n  configure vscode user settings file\n"
-VSCODE_CONFIG_FILE_CONTENTS='{
-    "window.zoomLevel": -2,
-    "window.menuBarVisibility": "default",
-    "files.exclude": {
-        "**/.git": true,
-        "**/.svn": true,
-        "**/.hg": true,
-        "**/CVS": true,
-        "**/.DS_Store": true,
-        "**/*.pyc": true
-    },
-    "editor.formatOnSave": true,
-    "editor.minimap.enabled": false,
-    "editor.quickSuggestions": {
-        "other": true,
-        "comments": true,
-        "strings": true
-    },
-    "go.useCodeSnippetsOnFunctionSuggest": true,
-    "go.autocompleteUnimportedPackages": true,
-    "go.useLanguageServer": true,
-    "python.linting.pylintArgs": [
-        "--load-plugins",
-        "pylint_django",
-        "--enable=E",
-        "--disable=W,R,C",
-        "--unsafe-load-any-extension=y"
-    ],
-    "python.formatting.autopep8Args": [
-        "--experimental",
-        "--in-place",
-        "-r",
-        "-aaaaaaaaaa"
-    ],
-    "python.formatting.blackArgs": [
-        "--line-length=100",
-        "--py36"
-    ],
-    "python.formatting.provider": "black",
-    "python.autoComplete.addBrackets": true,
-    "python.venvFolders": [
-        "envs",
-        ".pyenv",
-        ".direnv",
-        ".virtualenvs",
-        ".venv"
-    ],
-    "dart.allowAnalytics": false,
-    "dart.debugExternalLibraries": true,
-    "dart.debugSdkLibraries": true
-}'
-VSCODE_CONFIG_FILE=/home/komuw/.config/Code/User/settings.json
 mkdir -p /home/komuw/.config/Code/User
-touch "$VSCODE_CONFIG_FILE"
+touch /home/komuw/.config/Code/User/settings.json
 chown -R komuw:komuw /home/komuw/.config/Code/
-grep -qF -- "$VSCODE_CONFIG_FILE_CONTENTS" "$VSCODE_CONFIG_FILE" || echo "$VSCODE_CONFIG_FILE_CONTENTS" >> "$VSCODE_CONFIG_FILE"
+cp templates/vscode.j2 /home/komuw/.config/Code/User/settings.json
 
 printf "\n\n  install vscode extensions\n"
 export GOPATH="$HOME/go" && \
