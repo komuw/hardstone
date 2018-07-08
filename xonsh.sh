b5b49dda48d23dcea4aac3b645bce66805792fa0
Komu Wairagu <komuw05@gmail.com>
2018-07-08 22:10:48 +0300

    Templates (#11)
    * template copy
    
    * template copy

--- xonsh.sh
+++ xonsh.sh
@@ -15,24 +15,8 @@ printf "\n\n  install xonsh and dependencies\n"
 python3 -m pip install -U xonsh ptk prompt_toolkit==1.0.15 pygments
 
 printf "\n\n  configure xonsh.config.json.j2\n"
-XONSH_CONFIG_FILE_CONTENTS='# docs:
-# 1. http://xon.sh/envvars.html
-# 2. http://xon.sh/xonshrc.html
-# 3. https://github.com/gforsyth/dotfiles/blob/master/.xonshrc
-$SHELL_TYPE="prompt_toolkit"
-$XONSH_SHOW_TRACEBACK = True
-$XONSH_STORE_STDOUT = True
-$AUTO_SUGGEST = True
-$XONSH_CACHE_EVERYTHING = True
-$INDENT = "`·.,¸,.·*¯`·.,¸,.·*¯"
-$MULTILINE_PROMPT = "`·.,¸,.·*¯`·.,¸,.·*¯"
-$PATH = ["$HOME/.nvm/bin/", "$HOME/go/bin", "/usr/local/go/bin", "/usr/local/go/bin", "/usr/local/flutter/bin", "/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin:/bin", "/usr/games", "/usr/local/games", "/snap/bin", "/usr/local/lib/python2.7/dist-packages", "/usr/local/lib/python3.5/dist-packages", "/usr/local/lib/python3.6/dist-packages/", "/usr/lib/dart/bin", "/home/komuw/.pub-cache/bin"]
-$PROMPT = "{user}:{short_cwd}({curr_branch}{branch_color})>"
-$TITLE = "{short_cwd}:{curr_branch}"
-$XONSH_DATETIME_FORMAT = "%M:%H %d-%m-%Y"'
-XONSH_CONFIG_FILE=/home/komuw/.xonshrc
-touch "$XONSH_CONFIG_FILE"
-grep -qF -- "$XONSH_CONFIG_FILE_CONTENTS" "$XONSH_CONFIG_FILE" || echo "$XONSH_CONFIG_FILE_CONTENTS" >> "$XONSH_CONFIG_FILE"
+touch /home/komuw/.xonshrc
+cp templates/xonsh.j2 /home/komuw/.xonshrc
 
 printf "\n\n  add xonsh to shells\n"
 ETC_SHELLS_CONFIG_FILE_CONTENTS='/usr/local/bin/xonsh'
