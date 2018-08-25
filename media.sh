bc507e4895a90f0363776001edbdf73bfa43175a
Komu Wairagu <komuw05@gmail.com>
2018-08-25 15:37:47 +0300

    Create media.sh

---
+++ media.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
+    # Bash 4.4, Zsh
+    set -euo pipefail
+else
+    # Bash 4.3 and older chokes on empty arrays with set -u.
+    set -eo pipefail
+fi
+shopt -s nullglob globstar
+export DEBIAN_FRONTEND=noninteractive
+
+printf "\n\n  install acestreamplayer\n"
+snap install acestreamplayer
