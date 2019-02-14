77e4cac457cef2e1d2cfe18de0cf1c2e53397489
komuW <komuw05@gmail.com>
2019-02-14 13:05:18 +0300

    add java for mac

---
+++ java.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
+    # Bash 4.4, Zsh
+    set -euo pipefail
+else
+    # Bash 4.3 and older chokes on empty arrays with set -u.
+    set -eo pipefail
+fi
+shopt -s nullglob globstar
+export DEBIAN_FRONTEND=noninteractive
+
+printf "\n\n install Java openJDK\n"
+apt -y update
+apt -y install default-jre
+apt -y install default-jdk
