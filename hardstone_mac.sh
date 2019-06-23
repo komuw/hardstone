04cd63d5df7ca0efcf1be2ed18ea8e0d8cfe89f8
Komu Wairagu <komuw05@gmail.com>
2019-06-23 14:32:14 +0300

    update to openJDK 12

--- hardstone_mac.sh
+++ hardstone_mac.sh
@@ -160,7 +160,7 @@ chmod +x /usr/local/bin/gobin
 
 
 printf "\n\n install Java openJDK\n"
-wget -nc --directory-prefix=/tmp https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_osx-x64_bin.tar.gz
+wget -nc --directory-prefix=/tmp https://download.java.net/java/GA/jdk12.0.1/69cfe15208a647278a19ef0990eea691/12/GPL/openjdk-12.0.1_osx-x64_bin.tar.gz
 tar -xzf /tmp/openjdk-11.0.2_osx-x64_bin.tar.gz -C /Library/Java/JavaVirtualMachines/
 java -version
 javac -version
