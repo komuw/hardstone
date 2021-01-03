902789ee20e7689ee4f19d0226df3d3293cd6c97
komuW <komuw05@gmail.com>
2021-01-03 22:15:13 +0300

    d

--- nixos/start.sh
+++ nixos/start.sh
@@ -86,7 +86,165 @@ clear_stuff(){
 }
 clear_stuff
 
+
+
+echo "base-files
+apt
+base-passwd" >> old.txt
+
+echo "apt
+vlc
+base-passwd
+skype" >> new.txt
+
+cat old.txt | sort >> sorted_old.txt
+cat new.txt | sort >> sorted_new.txt
+diff sorted_old.txt sorted_new.txt | grep '>' | awk '{print $2}' # the packages to remove
+PACKAGES_TO_REMOVE=`diff sorted_old.txt sorted_new.txt | grep '>' | awk '{print $2}'`
+
 uninstall_non_essential_apt_packages(){
+    rm -rf /tmp/*.txt
+
+    # This are the packages that come with a clean install of ubuntu:20.04 docker image.
+    # We should not remove this, else bad things can happen.
+    echo "adduser
+apt
+base-files
+base-passwd
+bash
+bsdutils
+bzip2
+ca-certificates
+coreutils
+curl
+dash
+debconf
+debianutils
+diffutils
+dpkg
+e2fsprogs
+fdisk
+findutils
+gcc-10-base:amd64
+gpgv
+grep
+gzip
+hostname
+init-system-helpers
+krb5-locales
+libacl1:amd64
+libapt-pkg6.0:amd64
+libasn1-8-heimdal:amd64
+libattr1:amd64
+libaudit-common
+libaudit1:amd64
+libblkid1:amd64
+libbrotli1:amd64
+libbz2-1.0:amd64
+libc-bin
+libc6:amd64
+libcap-ng0:amd64
+libcom-err2:amd64
+libcrypt1:amd64
+libcurl4:amd64
+libdb5.3:amd64
+libdebconfclient0:amd64
+libext2fs2:amd64
+libfdisk1:amd64
+libffi7:amd64
+libgcc-s1:amd64
+libgcrypt20:amd64
+libgmp10:amd64
+libgnutls30:amd64
+libgpg-error0:amd64
+libgssapi-krb5-2:amd64
+libgssapi3-heimdal:amd64
+libhcrypto4-heimdal:amd64
+libheimbase1-heimdal:amd64
+libheimntlm0-heimdal:amd64
+libhogweed5:amd64
+libhx509-5-heimdal:amd64
+libidn2-0:amd64
+libk5crypto3:amd64
+libkeyutils1:amd64
+libkrb5-26-heimdal:amd64
+libkrb5-3:amd64
+libkrb5support0:amd64
+libldap-2.4-2:amd64
+libldap-common
+liblz4-1:amd64
+liblzma5:amd64
+libmount1:amd64
+libncurses6:amd64
+libncursesw6:amd64
+libnettle7:amd64
+libnghttp2-14:amd64
+libp11-kit0:amd64
+libpam-modules:amd64
+libpam-modules-bin
+libpam-runtime
+libpam0g:amd64
+libpcre2-8-0:amd64
+libpcre3:amd64
+libprocps8:amd64
+libpsl5:amd64
+libroken18-heimdal:amd64
+librtmp1:amd64
+libsasl2-2:amd64
+libsasl2-modules:amd64
+libsasl2-modules-db:amd64
+libseccomp2:amd64
+libselinux1:amd64
+libsemanage-common
+libsemanage1:amd64
+libsepol1:amd64
+libsmartcols1:amd64
+libsqlite3-0:amd64
+libss2:amd64
+libssh-4:amd64
+libssl1.1:amd64
+libstdc++6:amd64
+libsystemd0:amd64
+libtasn1-6:amd64
+libtinfo6:amd64
+libudev1:amd64
+libunistring2:amd64
+libuuid1:amd64
+libwind0-heimdal:amd64
+libzstd1:amd64
+login
+logsave
+lsb-base
+mawk
+mount
+ncurses-base
+ncurses-bin
+openssl
+passwd
+perl-base
+procps
+publicsuffix
+sed
+sensible-utils
+sudo
+sysvinit-utils
+tar
+tzdata
+ubuntu-keyring
+util-linux
+xz-utils
+zlib1g:amd64" >> /tmp/BASE_PACKAGES.txt
+    cat /tmp/BASE_PACKAGES.txt | sort >> /tmp/SORTED_BASE_PACKAGES.txt
+    printf "\n\n base packages are; \n"
+    cat /tmp/SORTED_BASE_PACKAGES.txt
+
+    ALL_CURRENTLY_INSTALLED_PACKAGES=`dpkg --get-selections | awk '{print $1}'`
+    printf "\n\n all currently installed packages; \n"
+    echo $ALL_CURRENTLY_INSTALLED_PACKAGES
+
+    diff /tmp/SORTED_BASE_PACKAGES.txt new.txt | grep '>' | awk '{print $2}' # to remove
+
+
     printf "\n\n 5. list all non-essential apt packages \n"
     echo `dpkg-query -Wf '${Package;-40}${Priority}\n' | grep -i optional | awk '{print $1}'`
 
@@ -94,6 +252,7 @@ uninstall_non_essential_apt_packages(){
     apt purge -y `dpkg-query -Wf '${Package;-40}${Priority}\n' | grep -i optional | awk '{print $1}'`
 }
 uninstall_non_essential_apt_packages
+
 
 
 
