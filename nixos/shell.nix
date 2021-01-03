70ef5eef674b7a9ed5d3e8c2e0b7fa72e6e0294a
komuW <komuw05@gmail.com>
2021-01-04 00:18:21 +0300

    d

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -1,5 +1,10 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz") {} }:
 
-https://nixos.org/guides/declarative-and-reproducible-developer-environments.html#declarative-reproducible-envs
+/*
+    1. https://nixos.org/manual/nix/stable/#chap-writing-nix-expressions
+    2. https://nixos.org/guides/declarative-and-reproducible-developer-environments.html#declarative-reproducible-envs
+*/
+
 pkgs.mkShell {
   buildInputs = [
     pkgs.which
