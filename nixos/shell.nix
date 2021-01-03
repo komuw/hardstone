96f955ebb0acf7049917ef5c69c564bab1459262
komuW <komuw05@gmail.com>
2021-01-04 00:19:29 +0300

    d

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -1,8 +1,13 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz") {} }:
 
 /*
+    docs:
     1. https://nixos.org/manual/nix/stable/#chap-writing-nix-expressions
     2. https://nixos.org/guides/declarative-and-reproducible-developer-environments.html#declarative-reproducible-envs
+
+    usage:
+    - from this directory, run;
+        /nix/var/nix/profiles/default/bin/nix-shell
 */
 
 pkgs.mkShell {
