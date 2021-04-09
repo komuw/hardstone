e4761406ec26d0094059f9b38b0b1571eaf24e60
komuW <komuw05@gmail.com>
2021-04-09 17:22:50 +0300

    p

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,9 +1,15 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4795e7f3a9cebe277bb4b5920caa8f0a2c313eb0.tar.gz") {} }:
+
+/*
+  An alternative that uses a pinned stable version is:
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz") {} }:
+*/
 
 /*
     docs:
     1. https://nixos.org/manual/nix/stable/#chap-writing-nix-expressions
     2. https://nixos.org/guides/declarative-and-reproducible-developer-environments.html#declarative-reproducible-envs
+    3. https://nixos.org/guides/towards-reproducibility-pinning-nixpkgs.html
 
     usage:
     - from this directory, run;
@@ -23,6 +29,3 @@ pkgs.mkShell {
     pkgs.micro
   ];
 }
-
-
-
