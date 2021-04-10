90d530f741d4515ee9c4fdb3b28df9153f0ecf14
komuW <komuw05@gmail.com>
2021-04-10 16:48:33 +0300

    m

--- nixos/pkgs/default.nix
+++ nixos/pkgs/default.nix
@@ -14,6 +14,7 @@ with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4795e7f3a9c
     2. https://nixos.org/guides/declarative-and-reproducible-developer-environments.html#declarative-reproducible-envs
     3. https://nixos.org/guides/towards-reproducibility-pinning-nixpkgs.html
     4. https://ghedam.at/15978/an-introduction-to-nix-shell
+    5. https://nix.dev/tutorials/declarative-and-reproducible-developer-environments.html
 
     usage:
     - from this directory, run;
