b0ea47529aa50efa4e33b3933df00d124a93a47a
komuW <komuw05@gmail.com>
2021-06-28 16:30:27 +0300

    28/06/2021

--- hardstone_mac.sh
+++ hardstone_mac.sh
@@ -56,40 +56,8 @@ cat /Users/komuw/.ssh/personal_id_rsa.pub
 
 
 printf "\n::CREATE dirs\n\n"
-mkdir -p /Users/komuw/swat
+mkdir -p /Users/komuw/work
 mkdir -p /Users/komuw/mystuff
-
-
-printf "\n::INSTALL golang\n\n"
-wget --output-document=/tmp/golang.pkg https://golang.org/dl/go1.16.darwin-amd64.pkg
-# this will install it inside /Applications/
-sudo installer -pkg /tmp/golang.pkg -target /
-
-
-printf "\n\n go install some golang packages\n"
-export PATH=$PATH:/usr/local/go/bin && \
-export PATH=$HOME/go/bin:$PATH
-go install github.com/rogpeppe/gohack@latest
-go install honnef.co/go/tools/cmd/staticcheck@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
-go install golang.org/x/tools/gopls@latest
-go install golang.org/x/tools/cmd/godex@latest
-go install github.com/traefik/yaegi/cmd/yaegi@latest # yaegi repl. usage: rlwrap yaegi
-go install github.com/maruel/panicparse/v2/cmd/pp@latest
-go install github.com/securego/gosec/cmd/gosec@latest
-go install github.com/google/pprof@latest
-go install github.com/rs/curlie@latest
-go install github.com/tsenart/vegeta@latest
-go install mvdan.cc/gofumpt@latest
-
-
-printf "\n::INSTALL dartlang \n\n"
-brew tap dart-lang/dart
-brew install dart --head
-brew update
-brew upgrade dart
-brew reinstall --force dart
-
 
 printf "\n::COPY conf files\n\n"
 cp templates/ssh_conf.j2 /Users/komuw/.ssh/config
@@ -133,24 +101,12 @@ echo '*.c     diff=cpp
 *.rs    diff=rust' >> ~/.gitattributes
 
 
-printf "\n\n install Java AWS Corretto openJDK\n"
-# java11 is an LTS
-if [[ ! -e /tmp/amazon_corretto.tar.gz ]]; then
-    wget --output-document=/tmp/amazon_corretto.tar.gz https://corretto.aws/downloads/latest/amazon-corretto-11-x64-macos-jdk.tar.gz
-fi
-mkdir -p /Library/Java/JavaVirtualMachines/
-sudo chown -R komuw:staff /Library/Java/JavaVirtualMachines/
-tar -xzf /tmp/amazon_corretto.tar.gz -C /Library/Java/JavaVirtualMachines/
-java -version
-javac -version
-brew install gradle
-
 printf "\n\n install ripgrep\n"
 if [[ ! -e /tmp/ripgrep.tar.gz ]]; then
-    wget --output-document=/tmp/ripgrep.tar.gz "https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep-12.1.1-x86_64-apple-darwin.tar.gz"
+    wget --output-document=/tmp/ripgrep.tar.gz "https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-apple-darwin.tar.gz"
 fi
 tar -xzf /tmp/ripgrep.tar.gz -C /tmp 
-mv /tmp/ripgrep-12.1.1-x86_64-apple-darwin/rg /usr/local/bin/rg
+mv /tmp/ripgrep-13.0.0-x86_64-apple-darwin/rg /usr/local/bin/rg
 chmod +x /usr/local/bin/rg
 
 printf "\n\n install ripgrep-all(rga)\n"
@@ -182,7 +138,6 @@ sudo chown -R komuw:staff /Users/komuw/.zshrc
 sudo chown -R komuw:staff ~/.oh-my-zsh
 
 
-
 printf "\n\n Install awscli \n"
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "/tmp/AWSCLIV2.pkg"
 # this will install it inside /Applications/
@@ -190,91 +145,141 @@ sudo installer -pkg /tmp/AWSCLIV2.pkg -target /
 
 printf "\n\n install kubectx\n"
 if [[ ! -e /tmp/kubectx.tar.gz ]]; then
-    wget --output-document=/tmp/kubectx.tar.gz "https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubectx_v0.9.1_darwin_x86_64.tar.gz"
+    wget --output-document=/tmp/kubectx.tar.gz "https://github.com/ahmetb/kubectx/releases/download/v0.9.3/kubectx_v0.9.3_darwin_x86_64.tar.gz"
 fi
 tar -xzf /tmp/kubectx.tar.gz -C /tmp
 mv /tmp/kubectx /usr/local/bin/kubectx
 chmod +x /usr/local/bin/kubectx
 
 printf "\n\n install poetry\n"
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/1.0.5/get-poetry.py | python3
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/1.1.7/get-poetry.py | python3
 
+install_golang(){
+    printf "\n::INSTALL golang\n\n"
+    wget --output-document=/tmp/golang.pkg https://golang.org/dl/go1.16.darwin-amd64.pkg
+    # this will install it inside /Applications/
+    sudo installer -pkg /tmp/golang.pkg -target /
 
-printf "\n\n install Pritunl\n"
-if [[ ! -e /tmp/pritunl.pkg.zip ]]; then
-    wget --output-document=/tmp/pritunl.pkg.zip "https://github.com/pritunl/pritunl-client-electron/releases/download/1.0.2440.93/Pritunl.pkg.zip"
-fi
-unzip /tmp/pritunl.pkg.zip -d /tmp/pritunl
-sudo installer -pkg /tmp/pritunl/Pritunl.pkg -target /
-
-
-printf "\n\n Add `ecr_login` bash helper function \n"
-BASHRC_PROFILE_FILE_CONTENTS="""
-function ecr_login () {
-  # login to AWS ECR.
-  local profile="\$1" # escape so that they remain
-  local aws_account_id="\$2"
-  aws --profile "\$profile" --region eu-west-1 ecr get-login-password | \
-  docker login --username AWS --password-stdin "\$aws_account_id".dkr.ecr.eu-west-1.amazonaws.com
-}"""
-BASHRC_PROFILE_FILE=/Users/komuw/.bash_profile
-if  ! grep -q "ecr_login" "$BASHRC_PROFILE_FILE"; then
-  echo "adding ecr_login bash helper function."
-  echo "$BASHRC_PROFILE_FILE_CONTENTS" >> "$BASHRC_PROFILE_FILE"
-fi
-
-
-install_zig() {
-    printf "\n\n remove any current zig files\n"
-    sudo rm -rf /usr/local/zigDir && sudo mkdir -p /usr/local/zigDir && sudo chown -R komuw /usr/local/zigDir
-
-    printf "\n\n  download zig from master branch(change when zig gets to ver1)\n"
-    # TODO: parse content from https://ziglang.org/download/index.json
-    LATEST_ZIG_MASTER=$(curl -s 'https://ziglang.org/download/index.json' | python3 -c "import sys, json; print(json.load(sys.stdin)['master']['x86_64-macos']['tarball'])")
-    printf "\nLATEST_ZIG_MASTER is: $LATEST_ZIG_MASTER\n"
-
-    wget -nc --output-document=/usr/local/zigDir/zig.tar.xz $LATEST_ZIG_MASTER
-    printf "\n\n  untar zig file\n"
-    tar -xf /usr/local/zigDir/zig.tar.xz -C /usr/local/zigDir --strip-components 1
-
-    # todo: add zig to $PATH
-    printf "\n\n use zig as: \n"
-    printf "\t /usr/local/zigDir/zig fmt .; /usr/local/zigDir/zig run main.zig \n"
-
-    # to install on Osx u can also use: brew install zig --HEAD
+    printf "\n\n go install some golang packages\n"
+    export PATH=$PATH:/usr/local/go/bin && \
+    export PATH=$HOME/go/bin:$PATH
+    go install github.com/rogpeppe/gohack@latest
+    go install honnef.co/go/tools/cmd/staticcheck@latest
+    go install github.com/go-delve/delve/cmd/dlv@latest
+    go install golang.org/x/tools/gopls@latest
+    go install golang.org/x/tools/cmd/godex@latest
+    go install github.com/traefik/yaegi/cmd/yaegi@latest # yaegi repl. usage: rlwrap yaegi
+    go install github.com/maruel/panicparse/v2/cmd/pp@latest
+    go install github.com/securego/gosec/cmd/gosec@latest
+    go install github.com/google/pprof@latest
+    go install github.com/rs/curlie@latest
+    go install github.com/tsenart/vegeta@latest
+    go install mvdan.cc/gofumpt@latest
 }
+install_golang
 
-install_zig
-
+install_dartlang(){
+    printf "\n::INSTALL dartlang \n\n"
+    brew tap dart-lang/dart
+    brew install dart --head
+    brew update
+    brew upgrade dart
+    brew reinstall --force dart
+}
+install_dartlang
 
 install_terraform(){
     printf "\n\n install terraform\n"
     tfenv install latest
-    tfenv install 0.13.5
+    tfenv install 1.0.1
     tfenv use latest
 }
 install_terraform
 
-install_gleamLang() {
-    GLEAM_VERSION="https://github.com/gleam-lang/gleam/releases/download/v0.14.3/gleam-v0.14.3-macos.tar.gz"
 
-    printf "\n\n remove any current gleam \n"
-    sudo rm -rf /usr/local/bin/gleam /tmp/gleam /tmp/gleam.tar.gz
+# install_ecr_login_bash_helper(){
+# printf "\n\n Add `ecr_login` bash helper function \n"
+# BASHRC_PROFILE_FILE_CONTENTS="""
+# function ecr_login () {
+#   # login to AWS ECR.
+#   local profile="\$1" # escape so that they remain
+#   local aws_account_id="\$2"
+#   aws --profile "\$profile" --region eu-west-1 ecr get-login-password | \
+#   docker login --username AWS --password-stdin "\$aws_account_id".dkr.ecr.eu-west-1.amazonaws.com
+# }"""
+# BASHRC_PROFILE_FILE=/Users/komuw/.bash_profile
+# if  ! grep -q "ecr_login" "$BASHRC_PROFILE_FILE"; then
+#   echo "adding ecr_login bash helper function."
+#   echo "$BASHRC_PROFILE_FILE_CONTENTS" >> "$BASHRC_PROFILE_FILE"
+# fi
+# }
+# install_ecr_login_bash_helper
 
-    printf "\n\n  download gleam \n"
-    wget -nc --output-document=/tmp/gleam.tar.gz "$GLEAM_VERSION"
+# install_pritunl(){
+#     printf "\n\n install Pritunl\n"
+#     if [[ ! -e /tmp/pritunl.pkg.zip ]]; then
+#         wget --output-document=/tmp/pritunl.pkg.zip "https://github.com/pritunl/pritunl-client-electron/releases/download/1.0.2440.93/Pritunl.pkg.zip"
+#     fi
+#     unzip /tmp/pritunl.pkg.zip -d /tmp/pritunl
+#     sudo installer -pkg /tmp/pritunl/Pritunl.pkg -target /
+# }
+# install_pritunl
 
-    printf "\n\n  untar gleam file\n"
-    tar -xvf /tmp/gleam.tar.gz -C /tmp/
+# install_java(){
+#     printf "\n\n install Java AWS Corretto openJDK\n"
+#     # java11 is an LTS
+#     if [[ ! -e /tmp/amazon_corretto.tar.gz ]]; then
+#         wget --output-document=/tmp/amazon_corretto.tar.gz https://corretto.aws/downloads/latest/amazon-corretto-11-x64-macos-jdk.tar.gz
+#     fi
+#     mkdir -p /Library/Java/JavaVirtualMachines/
+#     sudo chown -R komuw:staff /Library/Java/JavaVirtualMachines/
+#     tar -xzf /tmp/amazon_corretto.tar.gz -C /Library/Java/JavaVirtualMachines/
+#     java -version
+#     javac -version
+#     brew install gradle
+# }
+# install_java
 
-    printf "\n\n add gleam to PATH: \n"
-    chmod +x /tmp/gleam
-    mv /tmp/gleam /usr/local/bin/gleam
+# install_zig() {
+#     printf "\n\n remove any current zig files\n"
+#     sudo rm -rf /usr/local/zigDir && sudo mkdir -p /usr/local/zigDir && sudo chown -R komuw /usr/local/zigDir
 
-    printf "\n\n install erlang & rebar3 \n"
-    brew update
-    brew install erlang
-    brew install rebar3
-}
+#     printf "\n\n  download zig from master branch(change when zig gets to ver1)\n"
+#     # TODO: parse content from https://ziglang.org/download/index.json
+#     LATEST_ZIG_MASTER=$(curl -s 'https://ziglang.org/download/index.json' | python3 -c "import sys, json; print(json.load(sys.stdin)['master']['x86_64-macos']['tarball'])")
+#     printf "\nLATEST_ZIG_MASTER is: $LATEST_ZIG_MASTER\n"
 
-install_gleamLang
+#     wget -nc --output-document=/usr/local/zigDir/zig.tar.xz $LATEST_ZIG_MASTER
+#     printf "\n\n  untar zig file\n"
+#     tar -xf /usr/local/zigDir/zig.tar.xz -C /usr/local/zigDir --strip-components 1
+
+#     # todo: add zig to $PATH
+#     printf "\n\n use zig as: \n"
+#     printf "\t /usr/local/zigDir/zig fmt .; /usr/local/zigDir/zig run main.zig \n"
+
+#     # to install on Osx u can also use: brew install zig --HEAD
+# }
+# install_zig
+
+# install_gleamLang() {
+#     GLEAM_VERSION="https://github.com/gleam-lang/gleam/releases/download/v0.14.3/gleam-v0.14.3-macos.tar.gz"
+
+#     printf "\n\n remove any current gleam \n"
+#     sudo rm -rf /usr/local/bin/gleam /tmp/gleam /tmp/gleam.tar.gz
+
+#     printf "\n\n  download gleam \n"
+#     wget -nc --output-document=/tmp/gleam.tar.gz "$GLEAM_VERSION"
+
+#     printf "\n\n  untar gleam file\n"
+#     tar -xvf /tmp/gleam.tar.gz -C /tmp/
+
+#     printf "\n\n add gleam to PATH: \n"
+#     chmod +x /tmp/gleam
+#     mv /tmp/gleam /usr/local/bin/gleam
+
+#     printf "\n\n install erlang & rebar3 \n"
+#     brew update
+#     brew install erlang
+#     brew install rebar3
+# }
+# install_gleamLang
