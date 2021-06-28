c9c673487e1fc17728ea3516c8b183949a6c4363
komuW <komuw05@gmail.com>
2021-06-28 18:17:21 +0300

    linux 11

---
+++ digitalocean.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
+    # Bash 4.4, Zsh
+    set -euo pipefail
+else
+    # Bash 4.3 and older chokes on empty arrays with set -u.
+    set -eo pipefail
+fi
+shopt -s nullglob globstar
+
+export DEBIAN_FRONTEND=noninteractive && \
+apt -y update && \
+apt -y install python && \
+apt -y install python-pip nano wget unzip curl screen
