2aa0eca3f4c3f567ebf14d3010fefc5112fc08b8
komuW <komuw05@gmail.com>
2021-06-28 18:26:17 +0300

    linux 11

--- digitalocean.sh
+++ digitalocean.sh
@@ -12,3 +12,12 @@ export DEBIAN_FRONTEND=noninteractive && \
 apt -y update && \
 apt -y install python && \
 apt -y install python-pip nano wget unzip curl screen
+
+rm -rf $HOME/stuff
+mkdir -p $HOME/stuff
+cd $HOME/stuff
+
+wget -nc --output-document=$HOME/stuff/hardstone.zip https://github.com/komuw/hardstone/archive/refs/heads/master.zip
+unzip hardstone.zip
+mv hardstone-master/ hardstone
+cd $HOME/stuff/hardstone
