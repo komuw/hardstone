9e3f326c37585bcbc6c87eacb43a96cee9e7256a
komuW <komuw05@gmail.com>
2021-06-29 10:29:16 +0300

    m

--- linux/tools.sh
+++ linux/tools.sh
@@ -10,23 +10,30 @@ shopt -s nullglob globstar
 export DEBIAN_FRONTEND=noninteractive
 
 
-printf "\n\n install ripgrep\n"
-wget -nc --output-document=/tmp/ripgrep_amd64.deb "https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb"
-dpkg -i /tmp/ripgrep_amd64.deb
+install_ripgrep(){
+    printf "\n\n install ripgrep\n"
+    wget -nc --output-document=/tmp/ripgrep_amd64.deb "https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb"
+    dpkg -i /tmp/ripgrep_amd64.deb
+}
+install_ripgrep
 
+install_ripgrep_all(){
+    printf "\n\n install ripgrep-all(rga)\n"
+    if [[ ! -e /tmp/rga.tar.gz ]]; then
+        wget --output-document=/tmp/rga.tar.gz "https://github.com/phiresky/ripgrep-all/releases/download/v0.9.6/ripgrep_all-v0.9.6-x86_64-unknown-linux-musl.tar.gz"
+    fi
+    tar -xzf /tmp/rga.tar.gz -C /tmp
+    mv /tmp/ripgrep_all-v0.9.6-x86_64-unknown-linux-musl/rga /usr/local/bin/rga
+    chmod +x /usr/local/bin/rga
+}
+install_ripgrep_all
 
-printf "\n\n install ripgrep-all(rga)\n"
-if [[ ! -e /tmp/rga.tar.gz ]]; then
-    wget --output-document=/tmp/rga.tar.gz "https://github.com/phiresky/ripgrep-all/releases/download/v0.9.6/ripgrep_all-v0.9.6-x86_64-unknown-linux-musl.tar.gz"
-fi
-tar -xzf /tmp/rga.tar.gz -C /tmp
-mv /tmp/ripgrep_all-v0.9.6-x86_64-unknown-linux-musl/rga /usr/local/bin/rga
-chmod +x /usr/local/bin/rga
-
-
-printf "\n\n install mozilla rr debugger\n"
-wget -nc --output-document=/tmp/rr_amd64.deb "https://github.com/rr-debugger/rr/releases/download/5.4.0/rr-5.4.0-Linux-x86_64.deb"
-dpkg -i /tmp/rr_amd64.deb
+install_mozilla_rr(){
+    printf "\n\n install mozilla rr debugger\n"
+    wget -nc --output-document=/tmp/rr_amd64.deb "https://github.com/rr-debugger/rr/releases/download/5.4.0/rr-5.4.0-Linux-x86_64.deb"
+    dpkg -i /tmp/rr_amd64.deb
+}
+install_mozilla_rr
 
 install_zoom(){
     printf "\n\n install zoom\n"
