cf6b4424f55cffd58e3c32ab1c7168431a09902a
komuw <@gmail.com>
2021-07-01 22:59:55 +0300

    m

--- nixos/pkgs/golang.nix
+++ nixos/pkgs/golang.nix
@@ -16,26 +16,28 @@
 
     install_go_pkgs(){
         printf "\n\n go install some golang packages\n"
-        go install github.com/rogpeppe/gohack@latest
-        go install honnef.co/go/tools/cmd/staticcheck@latest
-        go install github.com/go-delve/delve/cmd/dlv@latest
-        go install golang.org/x/tools/gopls@latest
-        go install golang.org/x/tools/cmd/godex@latest
-        go install github.com/traefik/yaegi/cmd/yaegi@latest # yaegi repl. usage: rlwrap yaegi
-        go install github.com/maruel/panicparse/v2/cmd/pp@latest
-        go install github.com/securego/gosec/cmd/gosec@latest
-        go install github.com/google/pprof@latest
-        go install github.com/rs/curlie@latest
-        go install github.com/tsenart/vegeta@latest
-        go install mvdan.cc/gofumpt@latest
+
+        curlie_bin_file="/home/$MY_NAME/go/bin/curlie"
+        if [ -f "$curlie_bin_file" ]; then
+            # modules exists
+            echo ""
+        else
+            go install github.com/rogpeppe/gohack@latest
+            go install honnef.co/go/tools/cmd/staticcheck@latest
+            go install github.com/go-delve/delve/cmd/dlv@latest
+            go install golang.org/x/tools/gopls@latest
+            go install golang.org/x/tools/cmd/godex@latest
+            go install github.com/traefik/yaegi/cmd/yaegi@latest # yaegi repl. usage: rlwrap yaegi
+            go install github.com/maruel/panicparse/v2/cmd/pp@latest
+            go install github.com/securego/gosec/cmd/gosec@latest
+            go install github.com/google/pprof@latest
+            go install github.com/rs/curlie@latest
+            go install github.com/tsenart/vegeta@latest
+            go install mvdan.cc/gofumpt@latest
+            go install golang.org/dl/gotip@latest
+        fi
     }
     install_go_pkgs
-
-    install_gotip(){
-        printf "\n\n install gotip https://godoc.org/golang.org/dl/gotip \n"
-        go install golang.org/dl/gotip@latest
-    }
-    install_gotip
 
     change_owner(){
         printf "\n\n change ownership of ~/go\n"
@@ -47,3 +49,4 @@
 
   '';
 }
+
