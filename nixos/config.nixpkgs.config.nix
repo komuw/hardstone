86bf5bb65455ea4e115e4b0d9f79faadc17fe7b1
komuw <@gmail.com>
2021-07-02 10:33:11 +0300

    m

--- nixos/config.nixpkgs.config.nix
+++ nixos/config.nixpkgs.config.nix
@@ -1,7 +1,6 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz") {} }:
 
 {
-
 # The time zone used when displaying times and dates. 
 # See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for a comprehensive list of possible values for this setting. 
 # If null, the timezone will default to UTC and can be set imperatively using timedatectl.
