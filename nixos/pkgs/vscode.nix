b144a77c338360670e85598273450a12277b4cb3
$MY_NAME <komuw05@gmail.com>
2021-07-03 11:01:54 +0300

    nix

--- nixos/pkgs/vscode.nix
+++ nixos/pkgs/vscode.nix
@@ -36,16 +36,16 @@
             # extensions already installed
             echo ""
         else
-            code --user-data-dir='.' --install-extension ms-python.python
-            code --user-data-dir='.' --install-extension ms-python.vscode-pylance
-            code --user-data-dir='.' --install-extension dart-code.dart-code
-            code --user-data-dir='.' --install-extension dart-code.flutter
-            code --user-data-dir='.' --install-extension donaldtone.auto-open-markdown-preview-single
-            code --user-data-dir='.' --install-extension golang.go
-            code --user-data-dir='.' --install-extension ms-azuretools.vscode-docker
-            code --user-data-dir='.' --install-extension hashicorp.terraform
-            code --user-data-dir='.' --install-extension bbenoist.nix
-            # code --user-data-dir='.' --install-extension ms-vscode.cpptools
+            code --user-data-dir='/tmp/' --install-extension ms-python.python
+            code --user-data-dir='/tmp/' --install-extension ms-python.vscode-pylance
+            code --user-data-dir='/tmp/' --install-extension dart-code.dart-code
+            code --user-data-dir='/tmp/' --install-extension dart-code.flutter
+            code --user-data-dir='/tmp/' --install-extension donaldtone.auto-open-markdown-preview-single
+            code --user-data-dir='/tmp/' --install-extension golang.go
+            code --user-data-dir='/tmp/' --install-extension ms-azuretools.vscode-docker
+            code --user-data-dir='/tmp/' --install-extension hashicorp.terraform
+            code --user-data-dir='/tmp/' --install-extension bbenoist.nix
+            # code --user-data-dir='/tmp/' --install-extension ms-vscode.cpptools
         fi
     }
     install_vscode_extensions
