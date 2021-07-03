492c223f322b102de3f8a530a311d126ce32f8b1
$MY_NAME <komuw05@gmail.com>
2021-07-03 19:31:13 +0300

    bash

--- nixos/pkgs/setup_ssh.nix
+++ nixos/pkgs/setup_ssh.nix
@@ -73,32 +73,6 @@
         cp ../templates/ssh_conf.j2 /home/$MY_NAME/.ssh/config
     }
     setup_ssh_conf
-
-    setup_bashrc(){
-        # there ought to be NO newlines in the content
-        BASHRC_FILE_FILE_CONTENTS='
-
-        setup_ssh_agent(){
-            # start ssh-agent and add keys.
-            # https://stackoverflow.com/a/38619604
-            if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-                eval `ssh-agent`
-                ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-            fi
-            export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
-            ssh-add /home/$MY_NAME/.ssh/personal_id_rsa /home/$MY_NAME/.ssh/personal_work_id_rsa
-            ssh-add -l
-        }
-        setup_ssh_agent
-
-        export HISTTIMEFORMAT="%d/%m/%Y %T "'
-
-        BASHRC_FILE=/home/$MY_NAME/.bashrc
-        touch "$BASHRC_FILE"
-        grep -qxF "$BASHRC_FILE_FILE_CONTENTS" "$BASHRC_FILE" || echo "$BASHRC_FILE_FILE_CONTENTS" >> "$BASHRC_FILE"
-    }
-    setup_bashrc
-
   '';
 }
 
