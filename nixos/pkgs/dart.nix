4ce55e8a3d92b5371a10c9ccac3db65ecc0cd38f
$MY_NAME <komuw05@gmail.com>
2021-07-05 16:07:11 +0300

    m

--- nixos/pkgs/dart.nix
+++ nixos/pkgs/dart.nix
@@ -1,20 +1,22 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz") {} }:
+with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz") {});
 
-{
-  inputs = [
-      pkgs.dart
-      pkgs.flutter
-  ];
+let
 
-  hooks = ''
-      # set -e # fail if any command fails
-      # do not use `set -e` which causes commands to fail.
-      # because it causes `nix-shell` to also exit if a command fails when running in the eventual shell
+in stdenv.mkDerivation {
+    name = "dart";
 
-    printf "\n running hooks for dart.nix \n"
+    buildInputs = [
+        pkgs.dart
+        pkgs.flutter
+    ];
 
-    MY_NAME=$(whoami)
-  '';
+    shellHook = ''
+        # set -e # fail if any command fails
+        # do not use `set -e` which causes commands to fail.
+        # because it causes `nix-shell` to also exit if a command fails when running in the eventual shell
+
+      printf "\n running hooks for dart.nix \n"
+
+      MY_NAME=$(whoami)
+    '';
 }
-
-
