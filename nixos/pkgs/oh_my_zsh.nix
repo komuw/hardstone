0a09167aa69d90ddcf81b0e76695f7c1f7ac7bf4
$MY_NAME <komuw05@gmail.com>
2021-07-05 16:10:00 +0300

    m

--- nixos/pkgs/oh_my_zsh.nix
+++ nixos/pkgs/oh_my_zsh.nix
@@ -1,54 +1,46 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz") {} }:
+with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz") {});
 
-{
-  inputs = [
-      pkgs.zsh
-      ];
+let
 
-  programs.zsh.enable = true;
-  programs.zsh.histSize = 4000;
-  programs.zsh.autosuggestions.enable = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.enableBashCompletion = true;
+in stdenv.mkDerivation {
+    name = "oh_my_zsh";
 
-  programs.zsh.ohMyZsh.enable = true;
+    buildInputs = [
+        pkgs.zsh
+        ];
 
-  users.defaultUserShell = pkgs.zsh;
+    shellHook = ''
+        # set -e # fail if any command fails
+        # do not use `set -e` which causes commands to fail.
+        # because it causes `nix-shell` to also exit if a command fails when running in the eventual shell
 
-  hooks = ''
-      # set -e # fail if any command fails
-      # do not use `set -e` which causes commands to fail.
-      # because it causes `nix-shell` to also exit if a command fails when running in the eventual shell
+        printf "\n running hooks for oh_my_zsh.nix \n"
 
-    printf "\n running hooks for oh_my_zsh.nix \n"
+        MY_NAME=$(whoami)
 
-    MY_NAME=$(whoami)
+        install_ohmyzsh(){
+            oh_my_zsh_licence="/home/$MY_NAME/.oh-my-zsh/LICENSE.txt"
+            if [ -f "$oh_my_zsh_licence" ]; then
+                # repo exists
+                echo -n ""
+            else
+                rm -rf /home/$MY_NAME/.oh-my-zsh
+                git clone https://github.com/robbyrussell/oh-my-zsh.git /home/$MY_NAME/.oh-my-zsh
 
-    install_ohmyzsh(){
-        oh_my_zsh_licence="/home/$MY_NAME/.oh-my-zsh/LICENSE.txt"
-        if [ -f "$oh_my_zsh_licence" ]; then
-            # repo exists
-            echo -n ""
-        else
-            rm -rf /home/$MY_NAME/.oh-my-zsh
-            git clone https://github.com/robbyrussell/oh-my-zsh.git /home/$MY_NAME/.oh-my-zsh
+                git clone https://github.com/zsh-users/zsh-autosuggestions /home/$MY_NAME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+                git clone https://github.com/zsh-users/zsh-completions /home/$MY_NAME/.oh-my-zsh/custom/plugins/zsh-completions
 
-            git clone https://github.com/zsh-users/zsh-autosuggestions /home/$MY_NAME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-            git clone https://github.com/zsh-users/zsh-completions /home/$MY_NAME/.oh-my-zsh/custom/plugins/zsh-completions
+                cp ../templates/zshrc.j2 /home/$MY_NAME/.zshrc
+                cp ../templates/zshrc.j2 ~/.zshrc
+            fi
 
-            cp ../templates/zshrc.j2 /home/$MY_NAME/.zshrc
-            cp ../templates/zshrc.j2 ~/.zshrc
-        fi
+            chown -R $MY_NAME:$MY_NAME /home/$MY_NAME/.zshrc
+            chown -R $MY_NAME:$MY_NAME /home/$MY_NAME/.oh-my-zsh
 
-        chown -R $MY_NAME:$MY_NAME /home/$MY_NAME/.zshrc
-        chown -R $MY_NAME:$MY_NAME /home/$MY_NAME/.oh-my-zsh
+            # printf "\n activate zsh shell\n"
+            # chsh -s $(which zsh)
+        }
+        install_ohmyzsh
 
-        # printf "\n activate zsh shell\n"
-        # chsh -s $(which zsh)
-    }
-    install_ohmyzsh
-
-  '';
+    '';
 }
-
-
