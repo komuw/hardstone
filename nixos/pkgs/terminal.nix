79f31b042f9e8695cb1a254fa85a897a4a04d224
$MY_NAME <komuw05@gmail.com>
2021-07-05 16:11:30 +0300

    m

--- nixos/pkgs/terminal.nix
+++ nixos/pkgs/terminal.nix
@@ -1,24 +1,29 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz") {} }:
+with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz") {});
 
-{
-  inputs = [
-      pkgs.terminator
-      ];
-  hooks = ''
-      # set -e # fail if any command fails
-      # do not use `set -e` which causes commands to fail.
-      # because it causes `nix-shell` to also exit if a command fails when running in the eventual shell
+let
 
-    printf "\n running hooks for terminal.nix \n"
-    
-    MY_NAME=$(whoami)
+in stdenv.mkDerivation {
+    name = "terminal";
 
-    setup_terminator_conf(){
-        mkdir -p /home/$MY_NAME/.config
-        mkdir -p /home/$MY_NAME/.config/terminator
-        cp ../templates/terminator_config /home/$MY_NAME/.config/terminator/config
-    }
-    setup_terminator_conf
+    buildInputs = [
+        pkgs.terminator
+        ];
 
-  '';
+    shellHook = ''
+        # set -e # fail if any command fails
+        # do not use `set -e` which causes commands to fail.
+        # because it causes `nix-shell` to also exit if a command fails when running in the eventual shell
+
+      printf "\n running hooks for terminal.nix \n"
+
+      MY_NAME=$(whoami)
+
+      setup_terminator_conf(){
+          mkdir -p /home/$MY_NAME/.config
+          mkdir -p /home/$MY_NAME/.config/terminator
+          cp ../templates/terminator_config /home/$MY_NAME/.config/terminator/config
+      }
+      setup_terminator_conf
+
+    '';
 }
