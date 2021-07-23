dd506882414f44c8b81f793d938dd36ecf4bc655
$MY_NAME <komuw05@gmail.com>
2021-07-23 19:35:03 +0300

    hardeningDisable

--- nixos/pkgs/golang.nix
+++ nixos/pkgs/golang.nix
@@ -5,6 +5,21 @@ let
 in stdenv.mkDerivation {
     name = "golang";
 
+    # set/unset `NIX_HARDENING_ENABLE` env var; https://stackoverflow.com/a/27719330/2768067
+    # We need to unset this because `delve` debugger is failing to debug with some error.
+    #     ```
+    #     runtime/cgo
+    #     warning _FORTIFY_SOURCE requires compiling with optimization (-O)
+    #         |    ^~~~~~~
+    #     cc1: all warnings being treated as errors
+    #     exit status 2
+    #     ```
+    # see issue: https://github.com/NixOS/nixpkgs/issues/18995
+    #            https://github.com/ziglang/zig/wiki/Development-with-nix
+    # also see PR: https://github.com/NixOS/nixpkgs/pull/28029/files on how it is set
+    # and its default value
+    hardeningDisable = [ "all" ];
+
     buildInputs = [
         pkgs.go
       ];
