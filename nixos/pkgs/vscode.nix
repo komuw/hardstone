7adde2d6812325d6a89ab4b4cbdb716873b0e1dd
$MY_NAME <komuw05@gmail.com>
2022-07-06 03:49:48 +0300

    u

--- nixos/pkgs/vscode.nix
+++ nixos/pkgs/vscode.nix
@@ -1,4 +1,4 @@
-with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/01d4e58f598bcaf02e5a92a67a98afccecc94b0c.tar.gz") {});
+with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/2169ff54f191e4cd7a47c80207d9b58b7e50202a.tar.gz") {});
 
 let
 
