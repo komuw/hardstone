6fb1f9ba564aefaea667c0e5eab3a5264e3c3a57
$MY_NAME <komuw05@gmail.com>
2022-08-18 17:36:50 +0300

    f

--- linux/upgrade.sh
+++ linux/upgrade.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
     # Bash 4.4, Zsh
-    set -euo pipefail
+    set -xeuo pipefail
 else
     # Bash 4.3 and older chokes on empty arrays with set -u.
-    set -eo pipefail
+    set -xeo pipefail
 fi
 shopt -s nullglob globstar
 export DEBIAN_FRONTEND=noninteractive
