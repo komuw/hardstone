44d869a435972f1a9592a55e0f6a5e4f9274ba83
Komu Wairagu <komuw05@gmail.com>
2022-11-06 10:32:19 +0300

    Update nixpkgs nov6 (#65)
    update-nixpkgs-Nov6

--- nixos/pkgs/setup_ssh.nix
+++ nixos/pkgs/setup_ssh.nix
@@ -1,4 +1,4 @@
-with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/74bf39d3f5c72b0cfecdb7f14301ee71ead129ee.tar.gz") {});
+with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cdd3dda63b198675a4c5ed186a15b8fbd35d19a9.tar.gz") {});
 
 let
     # TODO: should we have a different passphrase per key?
