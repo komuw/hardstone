cdcaf7e3718449d4e4061a2ba22d59e8cf8f93b7
$MY_NAME <komuw05@gmail.com>
2022-11-06 08:56:29 +0300

    f

--- nixos/config.nixpkgs.config.nix
+++ nixos/config.nixpkgs.config.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/74bf39d3f5c72b0cfecdb7f14301ee71ead129ee.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/72158f45a2ca93e0b436d34c6f97956c3af068be.tar.gz") {} }:
 
 {
 # The time zone used when displaying times and dates. 
