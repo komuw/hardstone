4df5f1b8148e878c9445055427f636d9593ac1d7
komuw <komuw05@gmail.com>
2024-06-12 09:54:59 +0300

    update

--- config.nixpkgs.config.nix
+++ config.nixpkgs.config.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/0b36900606b9be03ccb554be867ae2405f6ba428.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f63489a7f0a548df967dc58d7d8fd18a0046d37d.tar.gz") {} }:
 
 {
 # The time zone used when displaying times and dates. 
